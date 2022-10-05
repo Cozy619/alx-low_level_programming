3abe697b6349c1b9e4cb3ac5bddbf72242fc375c
wisdom joshua <wisdomjoshua55@gmail.com>
2022-10-05 11:56:06 -0700

    atr concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,43 +1,48 @@
-#include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+int _strlen(const char *s);
 
 /**
- * str_concat - concatenates two strings.
- * @s1: first string.
- * @s2: second string.
- *
- * Return: pointer of an array of chars
+ * str_concat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer to concatenated string
  */
+
 char *str_concat(char *s1, char *s2)
 {
-	char *strout;
-	unsigned int i, j, k, limit;
+	int i = 0, j = 0;
+	char *output;
 
 	if (s1 == NULL)
-		s1 = "";
+		s1 = "\0";
 	if (s2 == NULL)
-		s2 = "";
+		s2 = "\0";
+	i = _strlen(s1);
+	j = _strlen(s2);
 
-	for (i = 0; s1[i] != '\0'; i++)
-		;
+	output = malloc((i + j) * sizeof(*s1) + 1);
 
-	for (j = 0; s2[j] != '\0'; j++)
-		;
-
-	strout = malloc(sizeof(char) * (i + j + 1));
-
-	if (strout == NULL)
-	{
-		free(strout);
+	if (output == 0)
 		return (NULL);
-	}
+	strcat(output, s1);
+	strcat(output, s2);
 
-	for (k = 0; k < i; k++)
-		strout[k] = s2[j];
+	return (output);
+}
+/**
+ * _strlen - get  the length of the string s
+ * @s: pointer to the string whose length is required
+ * Return: length of the string
+ */
+int _strlen(const char *s)
+{
+	int i = 0;
 
-	limit = j;
-	for (j = 0; j <= limit; k++, j++)
-		strout[k] = s2[j];
-
-	return (strout);
+	for (; s[i] != '\0'; i++)
+		;
+		return (i);
 }
